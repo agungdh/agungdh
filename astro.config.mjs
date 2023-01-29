4e81fe9a3ae8c4e9d36019eea951e6fa51e4c345
= <=>
2023-01-29 12:58:36 +0700

    add deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sitemap(), image(), alpinejs(), mdx()]
+});
